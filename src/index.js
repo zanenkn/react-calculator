b8bb60ade04d6e3f5ffeff2653959e89830d613c
zanenkn <zane.neikena@gmail.com>
2019-07-19 21:25:37 +0200

    Initial CSS setup, variables and import in index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'\
+import './index.css'
 
-ReactDOM.render(<div>Hello World!</div>, document.getElementById('root'));
+ReactDOM.render(
+  <div>Hello World!</div>, 
+  document.getElementById('root')
+);
