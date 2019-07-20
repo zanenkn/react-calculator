c3124c022c0bcac9ef600a8bcb298c38c059e8ad
zanenkn <zane.neikena@gmail.com>
2019-07-20 22:47:29 +0200

    Implementation code for Key rendering the keyValue

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import propTypes from 'prop-types'
 
-const Key = ({ keyAction, keyType, keyValue}) => <div className='key-container' />
+const Key = ({ keyAction, keyType, keyValue}) => (
+  <div className='key-container'>
+    <p className='key-value'>
+      {keyValue}
+    </p>
+  </div>
+)
 
 Key.propTypes = {
   keyAction: propTypes.func.isRequired,
