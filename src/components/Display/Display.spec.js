9ff28ee63363e2ff43de60e4edaf62ceccb2b844
zanenkn <zane.neikena@gmail.com>
2019-07-20 14:49:49 +0200

    Snapshot test for Display component

--- src/components/Display/Display.spec.js
+++ src/components/Display/Display.spec.js
@@ -6,6 +6,8 @@ describe('Display', () => {
   let wrapper
   beforeEach(() => wrapper = shallow(<Display displayValue={''} />))
 
+  it('should render correctly', () => expect(wrapper).toMatchSnapshot())
+
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1)
   })
