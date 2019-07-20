b1b3e785d0b054ceb96a6d9ee09e417742ddf714
zanenkn <zane.neikena@gmail.com>
2019-07-20 12:29:21 +0200

    Renders Calculator component into the App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './App.css'
+import Calculator from '../Calculator/Calculator'
 
-const App = () => <div className="app-container" />
+const App = () => (
+  <div className="app-container">
+    <Calculator />
+  </div>
+)
 
 export default App
