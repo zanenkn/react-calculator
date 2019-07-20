64ac4fb35491dcef640cba56f5791fc8b78e84bd
zanenkn <zane.neikena@gmail.com>
2019-07-20 14:23:33 +0200

    Creates a test for Display component, adds a test for Display component being rendered in the Calculator component

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import Calculator from './Calculator'
+import Display from '../Display/Display'
 
 describe('Calculator', () => {
   let wrapper
@@ -8,5 +9,9 @@ describe('Calculator', () => {
 
   it('should render a <div />', () => {
     expect(wrapper.find('div').length).toEqual(1)
+  })
+
+  it('should render the Display Component', () => {
+    expect(wrapper.containsMatchingElement(<Display />)).toEqual(true)
   })
 })
