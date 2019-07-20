2bc1adaed5335f64ad0a0d48b32e4ae99e75d5bd
zanenkn <zane.neikena@gmail.com>
2019-07-20 11:55:18 +0200

    Test for Calculator component rendered into the App component

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import App from './App'
-import { isTSAnyKeyword } from '@babel/types';
+import Calculator from '../Calculator/Calculator'
 
 describe('App', () => {
+  let wrapper;
+  beforeEach(() => wrapper = shallow(<App />))
+
   it('should render a <div/>', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find('div').length).toEqual(1);
+  })
+
+  it('should render the Calculator component', () => {
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true)
   })
 })
