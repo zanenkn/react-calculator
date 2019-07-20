9b1853d742ca8d36fa02051165f1c535e9fb01a0
zanenkn <zane.neikena@gmail.com>
2019-07-20 15:14:06 +0200

    Test for Keypad being rendered in Calculator component

--- src/components/Calculator/Calculator.spec.js
+++ src/components/Calculator/Calculator.spec.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Calculator from './Calculator'
 import Display from '../Display/Display'
+import Keypad from '../Keypad/Keypad'
 
 describe('Calculator', () => {
   let wrapper
@@ -11,9 +12,16 @@ describe('Calculator', () => {
     expect(wrapper.find('div').length).toEqual(1)
   })
 
-  it('should render the Display component', () => {
-    expect(wrapper.containsMatchingElement(
-      <Display displayValue={wrapper.instance().state.displayValue} />
-    )).toEqual(true)
+  it('should render the Display and Keypad components', () => {
+    expect(wrapper.containsAllMatchingElements([
+      <Display displayValue={wrapper.instance().state.displayValue} />,
+      <Keypad
+        callOperator={wrapper.instance().callOperator}
+        numbers={wrapper.instance().state.numbers}
+        operators={wrapper.instance().state.operators}
+        setOperator={wrapper.instance().setOperator}
+        updateDisplay={wrapper.instance().updateDisplay}
+      />
+    ])).toEqual(true)
   })
 })
