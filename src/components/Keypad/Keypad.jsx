dae0abd61836ef78b8e87b14b80078f8f9a3681f
zanenkn <zane.neikena@gmail.com>
2019-07-20 19:13:41 +0200

    Implementation code for Keypad rendering operator values

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -8,12 +8,17 @@ const Keypad = ({
   setOperator, 
   updateDisplay 
 }) => {
-  const numberKeys = numbers.map(number => <p key={number}>{number}</p>)
   
+  const numberKeys = numbers.map(number => <p key={number}>{number}</p>)
+  const operatorKeys = operators.map(operator => <p key={operator}>{operator}</p> )
+
   return(
     <div className='keypad-container'>
       <div className='numbers-container'>
         {numberKeys}
+      </div>
+      <div className='operators-container'>
+        {operatorKeys}
       </div>
     </div>
   )
