d4c0ad1d47e91d3b26ca7ebae6ef3ad940994756
zanenkn <zane.neikena@gmail.com>
2019-07-21 16:19:32 +0200

    Implementation code in Keypad component to render a Key for each number, operator and submit key

--- src/components/Keypad/Keypad.jsx
+++ src/components/Keypad/Keypad.jsx
@@ -11,8 +11,23 @@ const Keypad = ({
   updateDisplay 
 }) => {
   
-  const numberKeys = numbers.map(number => <p key={number}>{number}</p>)
-  const operatorKeys = operators.map(operator => <p key={operator}>{operator}</p> )
+  const numberKeys = numbers.map(number => (
+    <Key
+      key={number}
+      keyAction={updateDisplay}
+      keyType='number-key'
+      keyValue={number}
+    />)
+  )
+
+  const operatorKeys = operators.map(operator => (
+    <Key
+      key={operator}
+      keyAction={updateDisplay}
+      keyType='operator-key'
+      keyValue={operator} 
+    />)
+  )
 
   return(
     <div className='keypad-container'>
@@ -24,12 +39,14 @@ const Keypad = ({
       <div className='operators-container'>
         {operatorKeys}
       </div>
-
-      <Key 
-        keyAction={callOperator}
-        keyType=''
-        keyValue=''
-      />
+      
+      <div className='submit-container'>
+        <Key 
+          keyAction={callOperator}
+          keyType='submit-key'
+          keyValue='='
+        />
+      </div>
 
     </div>
   )
